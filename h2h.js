89b135bc9a5e4475ed8a542306ff3da0f389e58f
Primoz Cigler <primoz@proteusnet.com>
2015-07-08 14:33:56 +0200

    Initial commit.

---
+++ h2h.js
@@ -0,0 +1,70 @@
+Providers = new Mongo.Collection( 'providers' );
+
+if (Meteor.isClient) {
+  // counter starts at 0
+  Session.setDefault('sortAsc', {name: 1});
+
+  Template.providersTable.helpers({
+    providers: function () {
+      return Providers.find({}, {sort: Session.get('sortAsc')});
+    }
+  });
+
+  Template.providersTable.events({
+    'click .js-sort-col': function (ev) {
+      ev.preventDefault();
+
+      var $target = $(ev.currentTarget);
+
+      $target.parent()
+        .addClass('sorted-by-col')
+        .siblings().removeClass('sorted-by-col');
+
+      var ordering = {};
+      ordering[$target.data('col')] = 1;
+
+      if (_.isEqual( ordering, Session.get( 'sortAsc' ) )) {
+        ordering[$target.data('col')] = -1;
+      }
+
+      Session.set('sortAsc', ordering);
+    },
+  });
+
+  Template.provider.helpers({
+    priceDollars: function () {
+      return Array(this.price + 1).join('<i class="glyphicon glyphicon-usd"></i>');
+    },
+
+    signedVotes: function () {
+      return this.votes > 0 ? '+' + this.votes : this.votes;
+    },
+  });
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    if (Providers.find().count() === 0) {
+      _.each([{
+        name:     'Google Compute Engine',
+        votes:    17,
+        price:    4,
+        h2hscore: 54,
+      },
+      {
+        name:     'Digital Ocean',
+        votes:    13,
+        price:    3,
+        h2hscore: 23,
+      },
+      {
+        name:     'Amazon AWS',
+        votes:    -978,
+        price:    5,
+        h2hscore: -54,
+      }], function ( provider ) {
+        Providers.insert( provider );
+      } );
+    }
+  });
+}
